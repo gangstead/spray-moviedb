032565e5ae6cd6c60730eeda1f7b1ceee650d12f
ewoods <eric.woods@email.com>
2014-01-06 21:53:42 -0600

    Added API key placeholder

--- src/main/scala/com/example/util/GlobalConstants.scala
+++ src/main/scala/com/example/util/GlobalConstants.scala
@@ -1,7 +1,7 @@
 package com.example.util
 
 object GlobalConstants {
-  val API_KEY_QUERY_PARAM = "?api_key=056a4a18b7551b1a4b331eace783231f"
+  val API_KEY_QUERY_PARAM = "?api_key=putyourmoviedbapikeyhere"
   val REQUEST_TOKEN = "request_token"
   val MOVIEDB_AUTH_URL = "http://www.themoviedb.org/authenticate/"
 
